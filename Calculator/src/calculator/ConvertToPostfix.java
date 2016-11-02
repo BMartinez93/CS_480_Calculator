a5dac306f722bcae90a4f650f59c758327456836
Bryan Martinez <mbryan93@live.com>
2016-11-01 23:05:31 -0700

    Updated the code to accept multi digit numbers.

--- Calculator/src/calculator/ConvertToPostfix.java
+++ Calculator/src/calculator/ConvertToPostfix.java
@@ -1,65 +1,85 @@
 package calculator;
-import java.util.Stack;
+import java.util.*;
 
 /**
- *
- * @author Dasmann
+ * CS 480 - Vajda 
+ * Lab 3 
+ * Last Update: 1 November 2016
+ * @author Bryan Martinez
  */
 public class ConvertToPostfix {
-    
-    public static String convertToPostfix(String str){
-        Stack<Character> stack = new Stack<>();
-        StringBuilder result = new StringBuilder();
+
+    /**
+     * Simple method that converts infix to postfix
+     * 
+     * @param str string representation of a prefix equation
+     * @return ArrayList of type String with seperated values 
+     * in post fix form
+     */
+    public static ArrayList<String> convertToPostfix(String str){
+        Stack<String> stack = new Stack();
+        String[] preFix = str.split(" ");
+        ArrayList<String> result = new ArrayList();
         char c;
+        String value;
         
-        for (int i=0; i < str.length(); i++){
-            c = str.charAt(i);
+        for (int i=0; i < preFix.length; i++){
+            c = preFix[i].charAt(0);
+            value = preFix[i];
             
+            //if value is an operand, add to output
             if (!(isValidOperator(c)))
-                result.append(c);
+                result.add(value);
+            
             else{
-                if (stack.isEmpty() || stack.peek() == '(')
-                    stack.push(c);
+                if (stack.isEmpty() || stack.peek().charAt(0) == '(' 
+                        && c != ')')
+                    stack.push(value);
                 
                 else if (c == '(')
-                    stack.push(c);
+                    stack.push(value);
                 
                 else if (c == ')'){
-                    char next = stack.peek();
-                    while (next != '('){
-                        if (next == ')')
+                    String next = stack.peek();
+                    while (next.charAt(0) != '('){
+                        if (next.charAt(0) == ')')
                             stack.pop();
                         else {
-                            result.append(stack.pop());
+                            result.add(stack.pop());
                             next = stack.peek();
                         }
                     }
                     stack.pop();
                 }
                     
-                else if (getPriority(c) > getPriority(stack.peek()))
-                    stack.push(c);
+                else if (getPriority(c) > getPriority(stack.peek().charAt(0)))
+                    stack.push(value);
                 
-                else if (getPriority(c) == getPriority(stack.peek())){
-                    result.append(stack.pop());
-                    stack.push(c);
+                else if (getPriority(c) == getPriority(stack.peek().charAt(0))){
+                    result.add(stack.pop());
+                    stack.push(value);
                 }
                 
-                else if(getPriority(c) < getPriority(stack.peek())){
-                    //while (!stack.isEmpty())
-                    result.append(stack.pop());
+                else if(getPriority(c) < getPriority(stack.peek().charAt(0))){
+                    result.add(stack.pop());
                     i -= 1;
-                }
-                    
+                }       
             }
-            
         }
 
+        //pop whatever is left in stack into result
         while (!stack.isEmpty())
-            result.append(stack.pop());
-        return result.toString();
+            result.add(stack.pop());
+        return result;
     }
     
+    /**
+     * Takes in a char and determines the priority for the 
+     * postfix conversion
+     * 
+     * @param a char that represents a symbol
+     * @return integer representing priority of char a
+     */
     public static int getPriority(char a){
         switch (a){
             case '(':
@@ -79,6 +99,11 @@ public class ConvertToPostfix {
         }
     }
     
+    /**
+     * Check if the character is a supported operator
+     * @param c character that represents operator
+     * @return boolean
+     */
     public static boolean isValidOperator(char c){
         if (c == '^' || c == '*' || c == '/' || c == '+' || c == '-' ||
                 c == '(' || c == ')')
@@ -86,14 +111,11 @@ public class ConvertToPostfix {
         return false;
     }
     
-    public double calculatePostfix(String str){
-        
-        return 0.0;
-    }
-    
+    //Test the functionality of the postfix conversion.
     public static void main(String[] args){
-        String test = "A*(B+C*D)*(F+E)";
-        
-        System.out.println(convertToPostfix(test));
+        String test = "( ( ( ( 5 + 3 ) ) ) ) * ( ( 12 / 3 ) ) ";
+        ArrayList<String> testPrint = convertToPostfix(test);
+        for (String str : testPrint)
+            System.out.print(str + " ");
     }
 }
